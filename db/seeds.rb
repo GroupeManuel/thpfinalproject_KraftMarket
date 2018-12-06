c36ff15aac6cb184c03cea173db039a3aaa0d3c1
wiliamailysh <wiliamailysh@gmail.com>
2018-12-06 15:40:52 +0100

    PROGRESS Seeds modified to read order

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,24 +25,23 @@ item_status = ["draft", "published", "sold", "deleted"]
 order_status = ["payed", "being_shipped", "shipped"]
 
 
-100.times do 
+100.times do
 
     # Create instances of model Item
-
-    Item.create!(
-        title: Faker::StarWars.character, 
-        description: Faker::StarWars.quote, 
-        price: rand(10..100), 
-        status: item_status[rand(0..3)], 
-        category_id: rand(Category.first.id..Category.last.id), 
+ 
+    item = Item.create!(
+        title: Faker::StarWars.character,
+        description: Faker::StarWars.quote,
+        price: rand(10..100),
+        status: item_status[rand(0..3)],
+        category_id: rand(Category.first.id..Category.last.id),
         seller_id: rand(User.first.id..User.last.id)
         )
-
-    # Create instances of model Order if item sold
-
-    if Item.last.status == "sold" 
+ 
+    # Create order if item sold
+ 
+    if Item.last.status == "sold"
         Order.create!(
-            item_id: Item.last.id,
             buyer_id: rand(User.first.id..User.last.id),
             price: Item.last.price,
             status: order_status[rand(0..2)],
@@ -58,7 +57,10 @@ order_status = ["payed", "being_shipped", "shipped"]
             invoice_street2: "",
             invoice_postcode: Faker::Address.postcode,
             invoice_city: Faker::Address.city,
-            invoice_country: Faker::Address.country,
+            invoice_country: Faker::Address.country
             )
+ 
+        item.order_id = Order.last.id
+        item.save
     end
-end
+ end
