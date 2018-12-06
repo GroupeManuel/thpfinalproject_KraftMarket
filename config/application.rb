2a9512bd698a1d972f894b4bfaa394d351e55ff0
DamVan75 <manueltrinquet@gmail.com>
2018-12-06 17:52:23 +0100

    FIX BUG added initialize precompile in config/application.rb v2

--- config/application.rb
+++ config/application.rb
@@ -4,8 +4,6 @@ require "active_storage/engine"
 
 require 'rails/all'
 
-config.assets.initialize_on_precompile = false
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
