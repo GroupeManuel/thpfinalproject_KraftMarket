fe496677dd2c244f5e4917faa0a3ec77e22474c9
DamVan75 <manueltrinquet@gmail.com>
2018-12-06 17:49:44 +0100

    FIX BUG added initialize precompile in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require "active_storage/engine"
 
 require 'rails/all'
 
+config.assets.initialize_on_precompile = false
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,7 +14,7 @@ module KraftMarket
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
