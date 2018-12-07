4e782b194ebb9ae1ea11203cbe5db2bbc9d68cbf
DamVan75 <manueltrinquet@gmail.com>
2018-12-07 14:14:39 +0100

    FIX BUGS logo in navbar and credentials to access app

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,4 +3,4 @@ Rails.configuration.stripe = {
      :secret_key => Rails.application.credentials.stripe[:secret_key]
  }
 
- Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
