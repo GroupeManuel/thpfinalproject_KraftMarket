9e1434d18767872f66b2ea53738a86437516877c
hrivierefr <legsmower@gmail.com>
2018-12-08 12:24:22 +0100

    PROGRESS order_controller > Corrections Stripe et ajout payment_id

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,71 +1,88 @@
 class OrdersController < ApplicationController
 
   def create
-    # Order.create!(buyer_id: current_user.id, delivery_method: params_order[:delivery_method])
-    @new_order = Order.create!(buyer_id: current_user.id, 
-                  delivery_method: params_order[:delivery_method],
-                  delivery_entity: params_order[:delivery_entity],
-                  delivery_street: params_order[:delivery_street],
-                  delivery_street2: params_order[:delivery_street2],
-                  delivery_postcode: params_order[:delivery_postcode],
-                  delivery_city: params_order[:delivery_city],
-                  delivery_country: params_order[:delivery_country],
-                  delivery_instructions: params_order[:delivery_instructions],
-                  invoice_entity: params_order[:invoice_entity],
-                  invoice_street2: params_order[:invoice_street2],
-                  invoice_street: params_order[:invoice_street],
-                  invoice_postcode: params_order[:invoice_postcode],
-                  invoice_city: params_order[:invoice_city],
-                  invoice_country: params_order[:invoice_country],
-                  price: Item.find(session[:item_id])
-    )
+    #Variable to recall the item purchased
+    purchased_item = Item.find(params[:order][:item_id])
+    cart = purchased_item.cart
+    buyer = cart.buyer
+   
+   # Amount of the order in cents
+    amount = (purchased_item.price*100).to_i
+ 
+    begin
 
-      # Update the item to make appear the order 
-    Item.find(session[:item_id]).update(order_id: @new_order.id)
-
-     # Amount in cents
-     @amount = session[:item_price]
-    
-     customer = Stripe::Customer.create(
+    #Create a customer in the Stripe database
+    if buyer.stripe_id == nil
+      stripe_customer = Stripe::Customer.create(
        :email => params[:stripeEmail],
        :source  => params[:stripeToken]
      )
-   
-     charge = Stripe::Charge.create(
-       :customer    => customer.id,
-       :amount      => @amount,
-       :description => 'Rails Stripe customer',
-       :currency    => 'eur'
+      buyer.update(stripe_id:stripe_customer.id)
+    end
+    
+    #Charge the customer
+    charge = Stripe::Charge.create(
+       :customer    => buyer.stripe_id,
+       :amount      => amount,
+       :description => 'Charge #{@cart.buyer.email}(@cart.buyer_id) for item #{@purchased_item.id}',
+       :currency    => 'eur',
+       :metadata    => {purchased_item:purchased_item.id, buyer:cart.buyer_id}
      )
 
-   rescue Stripe::CardError => e
-     flash[:error] = e.message
-     redirect_to cart_path(current_user.cart.id) and return
+    #Rescue in case of payment error
+    rescue Stripe::CardError => e
+    # Since it's a decline, Stripe::CardError will be caught
+      body = e.json_body
+      err  = body[:error]
 
-    flash[:notice] = "Votre commande est bien passé"
-    redirect_to root_path
+      puts "Status is: #{e.http_status}"
+      puts "Type is: #{err[:type]}"
+      puts "Charge ID is: #{err[:charge]}"
+      # The following fields are optional
+      puts "Code is: #{err[:code]}" if err[:code]
+      puts "Decline code is: #{err[:decline_code]}" if err[:decline_code]
+      puts "Param is: #{err[:param]}" if err[:param]
+      puts "Message is: #{err[:message]}" if err[:message]
+      flash[:error] = e.message
+      redirect_back fallback_location:cart_path(cart.id) and return
+
+    rescue Stripe::ValidationError => e
+       flash[:error] = e.message
+     # Too many requests made to the API too quickly
+    end
+
+    #If payment succeeded, create the order with external parameters     
+    new_order = Order.create!(
+      buyer_id: cart.buyer_id, 
+      price: purchased_item.price,
+      status:'payed',
+      payment_id: charge.id
+      )
+
+    #Update with delivery and invoice information filled with the form
+    new_order.update(params_order)
+                  
+    # Update the item to empty the cart and make it sold
+    Item.find(purchased_item.id).update(cart_id: nil, order_id: new_order.id)
+
+    flash[:notice] = "Votre commande est confirmée. Vous avez reçu un email de confirmation"
+    redirect_to user_path(current_user.id)
+
   end
 
   def show
-    
   end
 
   private 
 
   def params_order
-    params.require(:order).permit(:delivery_method, 
-                                  :delivery_entity, 
-                                  :delivery_street, 
-                                  :delivery_street2, 
-                                  :delivery_postcode, 
-                                  :delivery_city, 
-                                  :delivery_country, 
-                                  :delivery_instructions, 
-                                  :invoice_entity, 
-                                  :invoice_street, 
-                                  :invoice_street2, 
-                                  :invoice_postcode, 
-                                  :invoice_city, 
-                                  :invoice_country)
+    params.require(:order).permit(
+      :delivery_entity, :delivery_street, :delivery_street2, :delivery_postcode, :delivery_city, :delivery_country, :delivery_instructions, :delivery_method, 
+      :invoice_entity, :invoice_street, :invoice_street2, :invoice_postcode, :invoice_city, :invoice_country
+      )
   end
+
+  def stripe_payment
+  end
+
 end
