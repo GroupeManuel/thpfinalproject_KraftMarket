d1fc8a06d1ab116f08d57ee171cc8f54f1fccb56
wiliamailysh <wiliamailysh@gmail.com>
2018-12-12 19:14:15 +0100

    PROGRESS Carousel in the about page quite ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ Item.delete_all
 Order.delete_all
 
     # Create instances of model User
-first_names = ["marcel", "olivier", "anais", "felix"]
+first_names = ["marcel", "olivier", "anais", "félix"]
 
 first_names.each do |prenom|
     User.create(
