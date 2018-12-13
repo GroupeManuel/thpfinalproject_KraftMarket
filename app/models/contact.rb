a4ca103f60bd1a996a9c381fc34d19994f0837bd
DamVan75 <manueltrinquet@gmail.com>
2018-12-13 16:43:54 +0100

    FIXED BUG 3eme ajout solution probleme envoi email via sendgrid on heroku v2

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,16 +1,16 @@
 class Contact < MailForm::Base
-  append :remote_ip, :user_agent, :session
+ # append :remote_ip, :user_agent, :session
 
   attribute :name, :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :topic,   :validate => true
-  attribute :message,   :validate => true
+  attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :topic, :validate => true
+  attribute :message, :validate => true
 
   def headers
     {
       :subject => "Contact Form",
       :to => "manueltrinquet@gmail.com",
-      :from => %("#{name}" <#{topic}> <#{email}>)
+      :from => %("#{name}" <#{email}>)
     }
   end
 
