c9f1ddb654efa886638bc838f0fa1c3c578445c5
DamVan75 <manueltrinquet@gmail.com>
2018-12-13 18:39:51 +0100

    PROGRESS tag removed after email order confirmation - but still not working on mailchimp, need to investigate

--- app/services/mailing.rb
+++ app/services/mailing.rb
@@ -1,5 +1,7 @@
 class Mailing
 
+	require 'digest'
+
 	def initialize
 		@gibbon = Gibbon::Request.new
 	end
@@ -12,9 +14,16 @@ class Mailing
 	end
 
 	# Coordonné par le controller order, une fois qu'un order est créé
+	# Attention gem gibbon doesn't know the "tag" element from mailchimp, need to use its old name "segment" instead
 	def new_buyer(email)
 		@gibbon.lists("c3c516a4aa").segments("11385").members.create(body: {email_address: email})
-		puts "fuck gibbon"
+		puts "going to sleep"
+		sleep(300)
+		puts "I've slept, thanks"
+		md5_email = Digest::MD5.hexdigest(email.downcase)
+		puts md5_email		
+		@gibbon.lists("c3c516a4aa").segments("11385").members(md5_email).delete
+		puts "okkkk"
 	end
 
 
@@ -26,7 +35,7 @@ class Mailing
 		@gibbon.automations.retrieve
 	end
 	def retrieve3
-		@gibbon.lists("c3c516a4aa")
+		@gibbon.lists("c3c516a4aa").members("5cef37a289449ccb30728bc711dbaf7d").email_address
 	end
 ######### NOT USEFUL FOR APP
 
@@ -36,3 +45,4 @@ end
 # https://github.com/amro/gibbon
 # https://developer.mailchimp.com/documentation/mailchimp/reference/lists/segments/
 # https://developer.mailchimp.com/documentation/mailchimp/guides/how-to-use-tags/
+
