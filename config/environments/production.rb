6bdff1ce5b4b5feecd452088d2e0b261cdec4bfd
DamVan75 <manueltrinquet@gmail.com>
2018-12-13 16:14:38 +0100

    FIXED BUG sendgrid login credentials on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['sendgrid_username'],
-    :password => ENV['sendgrid_password'],
+  :user_name => ENV['sendgrid_username'],
+  :password => ENV['sendgrid_password'],
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
