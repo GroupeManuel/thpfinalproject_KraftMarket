94e9aaf6d35e8310b4cd37bd31118c7151deb772
wiliamailysh <wiliamailysh@gmail.com>
2018-12-13 14:54:51 +0100

    PROGRESS Devise messages flashs modified

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,12 @@ module KraftMarket
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
+    config.i18n.default_locale = :fr
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
 end
+
+
