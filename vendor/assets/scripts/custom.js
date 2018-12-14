9cbacfba532f5e8d9427df0a35a37f79fe67119a
hrivierefr <legsmower@gmail.com>
2018-12-14 12:41:38 +0100

    PROGRESS navbar > retrait des conditions scrollTop

--- vendor/assets/scripts/custom.js
+++ vendor/assets/scripts/custom.js
@@ -74,16 +74,6 @@ $(document).ready(function(){
 	// sticky header script
 	var headerOffset = $("#header-container").height() * 2; // height on which the sticky header will shows
 
-//	$(window).scroll(function(){
-//		if($(window).scrollTop() >= headerOffset){
-//			$("#header.cloned").addClass('sticky').removeClass("unsticky");
-//			$("#navigation.style-2.cloned").addClass('sticky').removeClass("unsticky");
-//		} else {
-//			$("#header.cloned").addClass('unsticky').removeClass("sticky");
-//			$("#navigation.style-2.cloned").addClass('unsticky').removeClass("sticky");
-//		}
-//	});
-
 
 	/*----------------------------------------------------*/
 	/*  Back to Top
